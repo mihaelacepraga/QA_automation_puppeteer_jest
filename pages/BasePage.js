2bd5acc7027cc0c828f9d74dd9c2364c52e9021e
Cepraga Mihaela <mihaelacepraga@gmail.com>
2023-04-01 22:29:37 +0300

    create base page

---
+++ pages/BasePage.js
@@ -0,0 +1,11 @@
+export default class BasePage {
+    async wait(milliseconds) {
+        return new Promise(r => setTimeout(r, milliseconds));
+    };
+    async getTitle() {
+        return page.title();
+    };
+    async getUrl() {
+        return page.url();
+    };
+};
