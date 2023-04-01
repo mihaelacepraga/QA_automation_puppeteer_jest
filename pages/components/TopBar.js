26885c880b7def3aa5fd1a33599727713fa232be
Cepraga Mihaela <mihaelacepraga@gmail.com>
2023-04-01 22:32:27 +0300

    create topBar page

---
+++ pages/components/TopBar.js
@@ -0,0 +1,10 @@
+export default class TopBar {
+    async isTopBarDisplayed() {
+        await page.waitForSelector('div[data-id="6d9abfaa"]');
+        await page.waitForSelector(".login.top_bar_item");
+    }
+    async clickSignInBtn() {
+        await page.click(".login.top_bar_item");
+
+    }
+}
