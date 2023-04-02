c8b8bd133b5e035b5ab050862669af347193ec8b
Cepraga Mihaela <mihaelacepraga@gmail.com>
2023-04-02 14:03:35 +0300

    create account page

---
+++ pages/CreateAccountPage.js
@@ -0,0 +1,27 @@
+import BasePage from "./BasePage";
+
+export default class CreateAccountPage extends BasePage {
+    async visit() {
+        await page.click('i[class="fto-user icon_btn"]');;
+    };
+    async isCustumerFormDisplayed() {
+        await page.waitForSelector('input[name="firstname"]');
+        await page.waitForSelector('input[name="lastname"]');
+        await page.waitForSelector('#customer-form > div > div > div > div:nth-child(3) > div > div > input');
+        await page.waitForSelector('input[type="password"]');
+      
+
+    };
+    async createAccount(firstname, lastname, email, pass) {
+        await page.type('input[name="firstname"]', firstname);
+        await page.type('input[name="lastname"]', lastname);
+        await page.type('#customer-form > div > div > div > div:nth-child(3) > div > div > input', email);
+        await page.type('input[type="password"]', pass);
+        await page.waitForSelector('input[name="customer_privacy"]');
+        await page.click('input[name="customer_privacy"]')
+        await page.waitForSelector('button[data-link-action="save-customer"]');
+        await page.click('button[data-link-action="save-customer"]');
+
+
+    }
+}
